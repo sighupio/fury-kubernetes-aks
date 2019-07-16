9d4f45704329696fb6af766808413c33c6314736
Luca Novara <luca@sighup.io>
2019-07-16 19:21:13 +0200

    [modules/network] added bastion_public_ip output

--- modules/network/output.tf
+++ modules/network/output.tf
@@ -12,3 +12,8 @@ output "main_network_security_group_name" {
   description = "name of the security group that contains rules for the created main subnet"
   value       = "${azurerm_network_security_group.network.name}"
 }
+
+output "bastion_public_ip" {
+  description = "list of bastions' IP addressess"
+  value       = "${flatten(azurerm_public_ip.bastion.*.ip_address)}"
+}

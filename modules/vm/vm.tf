d0a5b7fbb43fb03adc968dcb20128702ea3e3c35
Luca Novara <luca@sighup.io>
2019-09-24 07:17:03 +0200

    fixed domain_name_label

--- modules/vm/vm.tf
+++ modules/vm/vm.tf
@@ -4,7 +4,7 @@ resource "azurerm_public_ip" "vm" {
   location            = "${data.azurerm_resource_group.main.location}"
   resource_group_name = "${data.azurerm_resource_group.main.name}"
   allocation_method   = "Static"
-  domain_name_label   = "${var.domain_name_label}"
+  domain_name_label   = "${var.domain_name_label}-${count.index+1}"
 }
 
 resource "azurerm_network_interface" "vm" {
